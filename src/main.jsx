cb9cd35ccf3c3d92a26410249bda4526045656a8
Gourav Makode <140704003+GT0SRT@users.noreply.github.com>
2025-08-22 12:55:03 +0530

    vercel insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import { StrictMode } from 'react'
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,6 +9,8 @@ import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StrictMode>
+    <Analytics />
+    <SpeedInsights/>
     <App />
   </StrictMode>
   </BrowserRouter>,
